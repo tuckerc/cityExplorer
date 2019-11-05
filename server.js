f242a46aed2a470feebb9b63c3ee22c7a746ca79
Chase Tucker <tuck8106@gmail.com>
2019-11-05 11:10:22 -0800

    location route working

--- server.js
+++ server.js
@@ -11,8 +11,23 @@ if (PORT === null || PORT === '') {
   PORT = 46352;
 }
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+function Location(city, geoData) {
+  this.search_query = city;
+  this.formatted_query = geoData.results[0].formatted_address;
+  this.latitude = geoData.results[0].geometry.location.lat;
+  this.longitude = geoData.results[0].geometry.location.lng;
+}
+
+app.get('/location', (req, res) => {
+  // send the users current location back to them
+  const geoData = require('./data/geo.json');
+  const city = req.query.data;
+  const locationData = new Location(city, geoData);
+  res.send(locationData);
+});
+
+app.get('*', (req, res) => {
+  res.send('Welcome to City Explorer');
 });
 
 app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
