9e20db5ae9c7beebc6573cb19cea19466228b4f4
Chase Tucker <tuck8106@gmail.com>
2019-11-05 11:36:41 -0800

    weather route working

--- server.js
+++ server.js
@@ -18,12 +18,32 @@ function Location(city, geoData) {
   this.longitude = geoData.results[0].geometry.location.lng;
 }
 
+function WeatherDay(city, forecast, time) {
+  this.city = city;
+  this.forecast = forecast;
+  this.time = time;
+}
+
 app.get('/location', (req, res) => {
   // send the users current location back to them
   const geoData = require('./data/geo.json');
   const city = req.query.data;
   const locationData = new Location(city, geoData);
   res.send(locationData);
+});
+
+app.get('/weather', (req, res) => {
+  // send the client next 8 days forecast
+  let days = [];
+  const weatherData = require('./data/darksky.json');
+  const city = req.query.data;
+  for(let i = 0; i < 8; i++) {
+    let time = weatherData.daily.data[i].time;
+    let summary = weatherData.daily.data[i].summary;
+    let newDay = new WeatherDay(city, summary, time);
+    days.push(newDay);
+  }
+  res.send(days);
 });
 
 app.get('*', (req, res) => {
