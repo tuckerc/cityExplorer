d99e9ea724cc6d129ab74cf6a590869dbaf4ab74
Chase Tucker <tuck8106@gmail.com>
2019-11-05 09:21:12 -0800

    server functional

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict'
+
+require('dotenv').config();
+
+let express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
