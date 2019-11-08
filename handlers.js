f5de7fb82ceb83e242583041d90f735678c06626
Chase Tucker <tuck8106@gmail.com>
2019-11-07 22:49:00 -0800

    movies working except caching

--- handlers.js
+++ handlers.js
@@ -21,7 +21,7 @@ function Movie(movie) {
   this.overview = movie.overview;
   this.average_votes = movie.vote_average;
   this.total_votes = movie.vote_count;
-  this.image_url = movie.poster_path;
+  this.image_url = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
   this.popularity = movie.popularity;
   this.released_on = movie.release_date;
 }
